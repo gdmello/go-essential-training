e4aad4627411748eb9b91a72889b71e6f6e566d2
Gavin D'Mello <gavin.dmello@points.com>
2020-11-20 16:38:38 -0500

    more sessions

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -8,13 +8,12 @@ func main() {
    for i:=1; i<21;i++ {
 	switch {
 		case  ((i%3) == 0) && ((i%5) == 0):
-			fmt.Println("fizz buzz (3,5)")
 		case  (i%3) == 0:
+			fmt.Println("fizz (3)")
 			fmt.Println("fizz (3)")
 		case (i%5) == 0:
 			fmt.Println("buzz (5)")
-		default:
-	                fmt.Println(i)
 	}
+	   fmt.Println(i)
    }
 }
---
+++ slices.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+   myslice :=  []string{"one","two","three"}
+   fmt.Println(myslice[1:])
+}
